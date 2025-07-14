b083549affe2f9494bfc91c408d6670f848b0a6b
Reinhard Mengelberg <rmengelb@f0r5s13.codam.nl>
2025-07-14 15:58:32 +0200

    fixed norm

--- src/main/helpers.c
+++ src/main/helpers.c
@@ -6,7 +6,7 @@
 /*   By: rmengelb <rmengelb@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/06/30 17:01:51 by rmengelb      #+#    #+#                 */
-/*   Updated: 2025/06/30 17:08:13 by rmengelb      ########   odam.nl         */
+/*   Updated: 2025/07/14 15:52:50 by rmengelb      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,35 @@ int	count_args(t_token *args)
 	}
 	return (count);
 }
+
+// void	print_commands(t_command *cmd)
+// {
+// 	int		cmd_num;
+// 	t_token	*arg;
+
+// 	cmd_num = 1;
+// 	while (cmd)
+// 	{
+// 		printf("Command %d:\n", cmd_num++);
+// 		printf("  Command: %s\n", cmd->cmd ? cmd->cmd->str : "NULL");
+// 		printf("  Arguments: ");
+// 		arg = cmd->args;
+// 		while (arg && arg->type == ARG)
+// 		{
+// 			printf("'%s' ", arg->str);
+// 			arg = arg->next;
+// 		}
+// 		printf("\n");
+// 		printf("  Input: %s\n",
+// 			cmd->input ? cmd->input->str : "No token(STDIN)");
+// 		if (cmd->input && cmd->input->next)
+// 			printf("  Input File: %s\n", cmd->input->next->str);
+// 		printf("  Output: %s\n",
+// 			cmd->output ? cmd->output->str : "No token(STDOUT)");
+// 		if (cmd->output && cmd->output->next)
+// 			printf("  Output File: %s\n", cmd->output->next->str);
+// 		printf("  Is Piped: %s\n", cmd->is_piped ? "Yes" : "No");
+// 		printf("  FDs: in=%d, out=%d\n", cmd->in_fd, cmd->out_fd);
+// 		cmd = cmd->next;
+// 	}
+// }
